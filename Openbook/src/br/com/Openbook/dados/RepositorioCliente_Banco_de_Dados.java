6faa3a91354b9b876435f4e562f29a1b71655808
Waltson <waltson.silva@hotmail.com>
2013-11-19 22:06:42 -0300

    Nessa classe, está os métodos de cliente, onde foi implementada a parte de casdastro de cliente.

--- Openbook/src/br/com/Openbook/dados/RepositorioCliente_Banco_de_Dados.java
+++ Openbook/src/br/com/Openbook/dados/RepositorioCliente_Banco_de_Dados.java
@@ -3,9 +3,54 @@ package br.com.Openbook.dados;
 import java.util.List;
 
 import br.com.Openbook.negocio.Cliente;
+import br.com.Openbook.negocio.Conexao;
 import br.com.Openbook.negocio.IRepositorioCliente;
+import br.com.Openbook.negocio.Livro;
 
 public class RepositorioCliente_Banco_de_Dados implements IRepositorioCliente {
+
+	private Conexao con;
+
+	public RepositorioCliente_Banco_de_Dados() {
+		con = new Conexao();
+		con.openDB("Openbook.sqlite");
+
+	}
+
+	@Override
+	public List<Cliente> pesquisarCliente(String cpf) throws Exception {
+		// TODO Auto-generated method stub
+		boolean retorno = false;
+
+		return null;
+	}
+
+	@Override
+	public void cadastrarCliente(Cliente novoCliente) throws Exception {
+		// TODO Auto-generated method stub
+		String sql = "";
+		sql = "INSERT INTO Openbook.Cliente("
+				+ "nome,endereco,bairro,cidade,estado,cep,telefone,cpf,sexo)"
+				+ "VALEUS(" + ClienteInfo(novoCliente) + ");";
+
+		con.executeUpdate(sql);
+
+	}
+
+	private String ClienteInfo(Cliente cliente) {
+		StringBuilder saida = new StringBuilder();
+		saida.append("'" + cliente.getNome() + "',");
+		saida.append("'" + cliente.getEndereco() + "',");
+		saida.append("'" + cliente.getBairro() + "',");
+		saida.append("'" + cliente.getCidade() + "',");
+		saida.append("'" + cliente.getEstado() + "',");
+		saida.append("'" + cliente.getCep() + "',");
+		saida.append("'" + cliente.getTel() + "',");
+		saida.append("'" + cliente.getCpf() + "',");
+		saida.append("'" + cliente.getSexo() + "',");
+		return saida.toString();
+
+	}
 
 	@Override
 	public boolean existeCliente(String cpf) throws Exception {
@@ -14,28 +59,15 @@ public class RepositorioCliente_Banco_de_Dados implements IRepositorioCliente {
 	}
 
 	@Override
-	public List<Cliente> pesquisarCliente(String cpf) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void cadastrarCliente(Cliente novoCliente) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void atualizarCliente(Cliente cliente) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void excluirCliente(String cpf) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
